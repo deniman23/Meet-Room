295dca3dd38e6cccad7d200aff67419d0d71eb89
danetolk <94922898+deniman23@users.noreply.github.com>
2022-08-17 16:18:26 +0300

    Add ASP.Net

---
+++ Meet-Room-diploma/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", async context => {
+    var fileStream = File.ReadAllText(path: "/Users/daniiltolkachev/Desktop/FileServer/index.html");
+
+    context.Response.Headers.ContentType = "text/html";
+    context.Response.StatusCode = 200;
+    context.Response.Headers.Add("My-header", "My-app");
+
+    await context.Response.WriteAsync(fileStream);
+});
+
+app.Run();
